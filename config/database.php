8a273a184b4088b851a60d7a73bee53bb3214abe
Kot <33439542+kotx@users.noreply.github.com>
2021-01-31 13:23:16 -0800

    Update database config to use a url

--- config/database.php
+++ config/database.php
@@ -4,10 +4,7 @@ return [
     'redis' => [
         'client' => 'predis',
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0
+            'url' => env('REDIS_URL', 'tcp://127.0.0.1:6379?database=0')
         ]
     ]
 ];
